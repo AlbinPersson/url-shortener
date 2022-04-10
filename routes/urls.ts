bacbaf6fb4f3ebbb0b055c41b7bb956c5d3177f5
albin <albinp2009@live.se>
2022-04-10 12:02:55 +0200

    get and post operations created for Url

--- routes/urls.ts
+++ routes/urls.ts
@@ -1,9 +1,36 @@
 import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import { Url, validate } from "models/Url";
 
 const router = express.Router();
 
-router.get("/", (req: Request, res: Response) => {
-  res.send("Funkar");
+router.get("/", async (req: Request, res: Response) => {
+  const urls = await Url.find();
+  return res.send(urls);
+});
+
+router.get("/:id", async (req: Request, res: Response) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id))
+    return res.status(400).send("Invalid Id");
+
+  const url = await Url.findById(req.params.id);
+  if (!url)
+    return res.status(404).send(`url with id: ${req.params.id} was not found`);
+
+  return res.send(url);
+});
+
+router.post("/", async (req: Request, res: Response) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.message);
+
+  let url = new Url({
+    originalUrl: req.body.originalUrl,
+  });
+
+  url = await url.save();
+
+  return res.status(201).send(url);
 });
 
 export default router;
