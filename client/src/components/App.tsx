4163cf34176ada54d7b0c12c33839893b2b54fde
albin <albinp2009@live.se>
2022-04-10 14:34:27 +0200

    created navbar with css

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,13 +1,17 @@
 import { Route, Switch } from "react-router-dom";
 import UrlForm from "components/UrlForm";
 import Redirect from "components/Redirect";
+import Navbar from "./Navbar";
 
 function App() {
   return (
-    <Switch>
-      <Route path="/:id" component={Redirect} />
-      <Route path="/" component={UrlForm} />
-    </Switch>
+    <>
+      <Navbar />
+      <Switch>
+        <Route path="/:id" component={Redirect} />
+        <Route path="/" component={UrlForm} />
+      </Switch>
+    </>
   );
 }
 
