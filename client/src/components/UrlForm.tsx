f4ce5020d65c6dc5fbb75bd1778c4c7a84e5338b
albin <albinp2009@live.se>
2022-04-11 18:27:09 +0200

    added validtime to form

--- client/src/components/UrlForm.tsx
+++ client/src/components/UrlForm.tsx
@@ -4,10 +4,16 @@ import styled from "styled-components";
 
 interface FormData {
   originalUrl: string;
+  validTime: number;
 }
 
 function UrlForm() {
-  const { register, handleSubmit, setValue } = useForm<FormData>();
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<FormData>();
 
   const onSubmit = handleSubmit(async (data) => {
     await addUrl(data);
@@ -20,9 +26,18 @@ function UrlForm() {
       <FormBox onSubmit={onSubmit}>
         <Input
           {...register("originalUrl", { required: true })}
-          id="originalUrl"
           placeholder="Enter your Url"
         />
+        {errors.originalUrl && <h3>Url is required </h3>}
+
+        <Label htmlFor="validTime">Valid time in minutes (optional)</Label>
+        <InputNumber
+          {...register("validTime", { min: 1, max: 1000 })}
+          id="validTime"
+          type="number"
+        />
+        {errors.validTime && <h3>min: 1 max: 1000</h3>}
+
         <Submit type="submit" />
       </FormBox>
     </Container>
@@ -37,6 +52,15 @@ const Container = styled.div`
   height: 90vh;
   width: 100vw;
 `;
+const Label = styled.label`
+  font-size: 25px;
+  font-weight: 900;
+  letter-spacing: 1px;
+  color: #141414;
+  :hover {
+    cursor: pointer;
+  }
+`;
 
 const FormBox = styled.form`
   display: grid;
@@ -46,25 +70,50 @@ const FormBox = styled.form`
   width: 60vw;
   border-radius: 10vh;
   box-shadow: 0px 0px 50px 18px #a7a28423;
+  & h3 {
+    color: #ff0000;
+  }
 `;
 
 const Input = styled.input`
-  height: 7vh;
+  height: 10vh;
   width: 50vw;
   background-color: transparent;
   outline: none;
-  border: none;
-  margin-top: 20vh;
+  margin-top: 10vh;
   font-size: 35px;
-  font-weight: 900;
+  font-weight: 800;
   letter-spacing: 1px;
   color: #141414;
   text-align: center;
+  border: solid 3px #141414;
+  border-radius: 10vh;
   ::placeholder {
     color: #141414;
   }
   :hover {
     cursor: text;
+  }
+`;
+
+const InputNumber = styled.input`
+  font-size: 35px;
+  font-weight: 800;
+  color: #141414;
+  text-align: center;
+  width: 13vw;
+  outline: none;
+  margin-bottom: 1vh;
+  background-color: transparent;
+  border: solid 3px #141414;
+  border-radius: 10vh;
+  ::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+  ::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
   }
 `;
 
@@ -74,7 +123,7 @@ const Submit = styled.input`
   border: none;
   color: #141414;
   font-size: 35px;
-  font-weight: 900;
+  font-weight: 800;
   letter-spacing: 1px;
   transition: all 0.1s ease-in-out;
 
