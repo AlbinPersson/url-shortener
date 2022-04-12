97094e66019475ccfe7c4c52307a86aacd023ab6
albin <albinp2009@live.se>
2022-04-12 17:43:56 +0200

    added put and delete operations

--- models/Url.ts
+++ models/Url.ts
@@ -6,7 +6,7 @@ export const Url = mongoose.model(
   "Url",
   new mongoose.Schema({
     originalUrl: { type: String, maxlength: 1000, required: true },
-    validTime: { type: Date, required: true },
+    validTime: { type: Date },
   })
 );
 
@@ -19,6 +19,13 @@ export function validate(url: IUrl) {
       .max(1000)
       .required(),
     validTime: Joi.number().min(1).max(1000),
+  });
+
+  return schema.validate(url);
+}
+export function validateUpdate(url: IUrl) {
+  const schema = Joi.object({
+    validTime: Joi.number().min(1).max(1000).required(),
   });
 
   return schema.validate(url);
--- routes/urls.ts
+++ routes/urls.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
-import { Url, validate } from "models/Url";
+import { Url, validate, validateUpdate } from "models/Url";
 
 const router = express.Router();
 
@@ -18,8 +18,6 @@ router.get("/:id", async (req: Request, res: Response) => {
     return res.status(404).send(`url with id: ${req.params.id} was not found`);
 
   if (url.validTime && url.validTime < new Date()) {
-    await Url.findByIdAndDelete(req.params.id);
-
     return res.status(400).send("url has expired");
   }
 
@@ -43,6 +41,33 @@ router.post("/", async (req: Request, res: Response) => {
   url = await url.save();
 
   return res.status(201).send(url);
+});
+
+router.put("/:id", async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id))
+    return res.status(400).send("Invalid Id");
+
+  const { error } = validateUpdate(req.body);
+  if (error) return res.status(400).send(error.message);
+
+  const url = await Url.findById(req.params.id);
+  if (!url) return res.status(404).send("Not Found");
+
+  url.validTime = new Date().getTime() + req.body.validTime * 60000;
+
+  await url.save();
+
+  return res.send(url);
+});
+
+router.delete("/:id", async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id))
+    return res.status(400).send("Invalid Id");
+
+  const url = await Url.findByIdAndDelete(req.params.id);
+  if (!url) return res.status(404).send("Not Found");
+
+  return res.send(url);
 });
 
 export default router;
